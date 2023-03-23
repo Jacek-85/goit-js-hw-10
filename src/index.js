891a342a70986e3a08061490752082bcde612763
Jacek_Blaszczyk <111233513+Jacek-85@users.noreply.github.com>
2023-03-23 21:31:42 +0100

    v1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './fetchCountries';
 
+
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
